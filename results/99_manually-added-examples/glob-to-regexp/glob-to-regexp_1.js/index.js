937a89a919814372cae8ea424d88edd8f7eaf87f
Fernando Cristiani <fer5cristiani@gmail.com>
2021-05-21 15:18:02 +0200

    Rename 'manually added' section

--- results/99_manually-added-examples/glob-to-regexp/glob-to-regexp_1.js/index.js
+++ results/99_manually-added-examples/glob-to-regexp/glob-to-regexp_1.js/index.js
@@ -1,4 +1,4 @@
-var globToRegExp = require('glob-to-regexp');
+var globToRegExp = require("glob-to-regexp");
 var re = globToRegExp("p*uck");
 re.test("pot luck"); // true
 re.test("pluck"); // true
@@ -8,11 +8,16 @@ re = globToRegExp("*.min.js");
 re.test("http://example.com/jquery.min.js"); // true
 re.test("http://example.com/jquery.min.js.map"); // false
 
-re = globToRegExp("*/www/*.js", {});
+re = globToRegExp("*/www/*.js");
 re.test("http://example.com/www/app.js"); // true
 re.test("http://example.com/www/lib/factory-proxy-model-observer.js"); // true
 
-// Extended globs
-re = globToRegExp("*/www/{*.js,*.html}", { extended: true, flags: "i", globstar: true });
+// Manually added
+re = globToRegExp("*/www/*.js", {});
+re = globToRegExp("*/www/{*.js,*.html}", {
+  extended: true,
+  flags: "i",
+  globstar: true,
+});
 re.test("http://example.com/www/app.js"); // true
 re.test("http://example.com/www/index.html"); // true
